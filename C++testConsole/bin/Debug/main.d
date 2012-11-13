4de08bead0f19fcdb5c31e73ffb4de66cb8027e4
root <root@joris-ThinkPad-X201-Tablet.(none)>
2012-11-13 12:19:20 +0100

    initial commit comment

---
+++ C++testConsole/bin/Debug/main.d
@@ -0,0 +1,2 @@
+/root/Desktop/C++testConsole/C++testConsole/bin/Debug/main.o: \
+ /root/Desktop/C++testConsole/C++testConsole/main.cpp
---
+++ C++testConsole/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/thread/thread.hpp>
+#include <iostream>
+//using namespace std;
+
+
+void thread_function()
+{
+std::cout << "hey" << std::endl;
+}
+int main (int argc, char *argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+	boost::thread threadone(&thread_function);
+	threadone.join();
+	return 0;
+}
+
