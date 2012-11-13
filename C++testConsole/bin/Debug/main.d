39beb4c8b651a92265b9cb2c1f2bc3d5b8403628
root <root@joris-ThinkPad-X201-Tablet.(none)>
2012-11-13 14:54:59 +0100

    hoi

--- C++testConsole/bin/Debug/main.d
+++ C++testConsole/bin/Debug/main.d
@@ -1,2 +1,3 @@
 /root/Desktop/C++testConsole/C++testConsole/bin/Debug/main.o: \
- /root/Desktop/C++testConsole/C++testConsole/main.cpp
+ /root/Desktop/C++testConsole/C++testConsole/main.cpp \
+ /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
--- C++testConsole/main.cpp
+++ C++testConsole/main.cpp
@@ -1,17 +1,68 @@
 #include <boost/thread/thread.hpp>
 #include <iostream>
-//using namespace std;
-
-
+#include <stdio.h> 
+#include <cv.h> 
+#include <highgui.h> 
+////using namespace std;
+//
+//
 void thread_function()
 {
-std::cout << "hey" << std::endl;
-}
-int main (int argc, char *argv[])
-{
-	std::cout << "Hello world! V2" << std::endl;
-	boost::thread threadone(&thread_function);
-	threadone.join();
-	return 0;
-}
+CvCapture *capture = 0; 
+ IplImage *frame = 0; 
+ int key = 0;
+ capture = cvCaptureFromCAM(0); // use GUVCVIEW to determine videonumber (listed as video#) 
 
+ 
+
+ if(!capture) 
+
+ { 
+ fprintf(stderr, "Cannot open initialize webcam!\n"); 
+ return; 
+ } 
+
+ cvNamedWindow("result"); 
+ while(key != 'q') 
+ { 
+
+ frame = cvQueryFrame(capture); 
+ if(!frame) break; 
+
+ 
+
+ cvShowImage("result", frame); 
+
+ 
+
+ key = cvWaitKey(1); // leave this one out and OpenCV will not refresh it's window! 
+
+ } 
+
+ 
+
+ cvDestroyWindow("result"); 
+ cvReleaseCapture(&capture); 
+ return; 
+}
+//int main (int argc, char *argv[])
+//{
+//	std::cout << "Hello world! V2" << std::endl;
+//	boost::thread threadone(&thread_function);
+//	threadone.join();
+//	return 0;
+//}
+//
+//////////////////////////////////////////////////////////////////////////  
+
+
+ 
+
+ int main(int argc, char **argv) 
+ {
+ boost::thread threadone(&thread_function);
+threadone.join();
+return 0;
+ } 
+
+ //////////////////////////////////////////////////////////////////////////  
